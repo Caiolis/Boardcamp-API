5fd84caef791c9379d0ce3bd2ed310b120862d24
Caio Carvalho <kaiogesteira@gmail.com>
2023-05-15 17:35:09 -0300

    Deploy configs

--- src/app.js
+++ src/app.js
@@ -3,9 +3,10 @@ import router from "./routes/index.routes.js";
 
 //server Configs
 const app = express();
-PORT = 5000;
 
+dotev.config()
 app.use(json());
 app.use(router);
 
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
