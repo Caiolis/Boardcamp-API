3800196a377ecda8bf359db136024da548daead0
Caio Carvalho <kaiogesteira@gmail.com>
2023-05-15 16:55:10 -0300

    Customer operations

---
+++ src/schemas/customer.schema.js
@@ -0,0 +1,10 @@
+import joi from "joi";
+
+const customerSchema = joi.object({
+	name: joi.string().required().min(1),
+	phone: joi.string().required().min(10).max(11),
+	cpf: joi.string().required().regex(/^\d{11}$/),
+	birthday: joi.date().iso().required().raw(),
+});
+
+export default customerSchema;
